5348627e0af4f9d8d0f8fae9b77b8a58233defbd
sabbb <61593726+theycallmesabb@users.noreply.github.com>
2026-04-16 22:37:01 +0530

    Time: 0 ms (100%), Space: 15 MB (95.82%) - LeetHub

---
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    int search(vector<int>& arr, int target) {
+        int low = 0;
+        int high = arr.size()-1;
+        while(low<=high){
+            int mid = low+(high-low)/2;
+            if(arr[mid]==target)return mid;
+            //left hand sorted
+            if(arr[low]<=arr[mid]){
+                    if(arr[low]<=target && target<=arr[mid]){
+                        high = mid-1;
+                    }
+                    else{
+                        low= mid+1;
+                    }
+            }
+            //right hand is sorted
+            else{
+                if(arr[mid]<=target && target<=arr[high]){
+                        low=mid+1;
+                    }
+                    else{
+                        high= mid-1;
+                    }
+            }
+        }
+        return -1;
+    }
+};
