ef3fdd7e41d2e38610a3ca200625cd679da5cfd4
sabbb <61593726+theycallmesabb@users.noreply.github.com>
2026-06-03 19:25:07 +0545

    Time: 0 ms (100%), Space: 8.4 MB (45.42%) - LeetHub

--- 2315-count-asterisks/2315-count-asterisks.cpp
+++ 2315-count-asterisks/2315-count-asterisks.cpp
@@ -1,14 +1,12 @@
 class Solution {
 public:
     int countAsterisks(string s) {
-        bool inside = false;
+       int bar=0;
         int count = 0;
         for(char c : s){
-            if(c=='|') {
-                inside = !inside;
-            }
-            else if(c=='*' && !inside){
-                count++;
+            if(c=='|')bar++;
+            else{
+           if(bar%2==0 && c=='*') count++;
             }
         }
         return count;
