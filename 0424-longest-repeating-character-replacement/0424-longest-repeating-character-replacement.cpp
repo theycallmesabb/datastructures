e8f742eac21db413daaaba1141b15146415ae65c
sabbb <61593726+theycallmesabb@users.noreply.github.com>
2026-08-04 11:39:13 +0545

    Time: 31 ms (6.9%), Space: 11 MB (42.2%) - LeetHub

---
+++ 0424-longest-repeating-character-replacement/0424-longest-repeating-character-replacement.cpp
@@ -0,0 +1,39 @@
+class Solution {
+public:
+int find(vector<int> &a)
+{
+   int maxc=-1;
+   for(int i=0;i<256;i++)
+   maxc=max(maxc,a[i]);
+   return maxc;
+}
+
+    int characterReplacement(string s, int k) {
+        int n=s.size();
+        vector<int> f(256,0);
+        int low = 0, high = 0;
+        int res = INT_MIN;
+        for(int high=0;high<n;high++){
+            f[s[high]]++;
+            
+            int maxcnt = find(f);
+            int len=high-low+1;
+           int diff=len-maxcnt;
+           while(diff>k)
+           {
+               f[s[low]]--;
+               low++;
+               maxcnt=find(f);
+               len=high-low+1;
+               diff=len-maxcnt;
+           }
+          
+           res=max(res,len);
+       }
+       return res;
+
+
+        }
+
+    
+};
