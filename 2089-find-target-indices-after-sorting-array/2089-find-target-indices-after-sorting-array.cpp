a0bc40313872fda024c89b895d4a52a5afe562be
sabbb <61593726+theycallmesabb@users.noreply.github.com>
2026-08-18 09:44:03 +0545

    Time: 0 ms (100%), Space: 15.3 MB (16.73%) - LeetHub

--- 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
+++ 2089-find-target-indices-after-sorting-array/2089-find-target-indices-after-sorting-array.cpp
@@ -5,7 +5,6 @@ public:
         sort(nums.begin(),nums.end());
         for(int i=0;i<nums.size();i++){
             if(nums[i]==target) ans.push_back(i);
-            
         }
         return ans;
     }
